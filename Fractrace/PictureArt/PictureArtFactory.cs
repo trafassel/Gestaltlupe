9dc1d9f1458f7071215a2dddf7970107583eafdd
unknown <Per@.(none)>
2010-01-25 00:17:39 +0100

    Picture Art als Modul mit verschiedenen Renderern vorbereitet.

---
+++ Fractrace/PictureArt/PictureArtFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Fractrace;
+
+namespace Fractrace.PictureArt {
+    class PictureArtFactory {
+
+        public static PictureArt Create() {
+            
+            return new PictureArt();
+
+        }
+    }
+
+  
+}
