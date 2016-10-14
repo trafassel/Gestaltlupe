c686a68a9ed9df44fa04ed4a81d10a8a44c7ab73
Erp Trafassel <trafassel@googlemail.com>
2016-10-14 02:58:02 +0200

    Add Gestaltlupe Webpage as Asp.Net Core project.

---
+++ Gestalten/src/Gestalten/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestalten.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Documentation()
+        {
+            ViewData["Message"] = "....";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
